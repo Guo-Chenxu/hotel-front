b22f712965707235ffe7f66cce25f8a95f2d566e
hvinci <93910286+hvinci@users.noreply.github.com>
2024-03-21 15:20:00 +0800

    add router config

--- hotelfront/src/main.js
+++ hotelfront/src/main.js
@@ -1,12 +1,4 @@
-/*
- * @Author: hvinci
- * @Date: 2024-03-21 14:39:13
- * @LastEditors: hvinci
- * @LastEditTime: 2024-03-21 14:48:06
- * @Description: 
- * 
- * Copyright (c) 2024 by ${hvinci}, All Rights Reserved. 
- */
+
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
