2f102c08f8f7a7462cac22d428c8fdf6196b49ed
Rablady <116176222+Rablady@users.noreply.github.com>
2024-03-31 21:54:09 +0800

    Add files via upload

---
+++ hotelfront/src/components/UserMain.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="user-main">
+        <img src="../assets/userview.png" style="width: 100%; height: 100%; opacity: 0.5;">
+        <div class="rectangle">
+            <div class="rectangle1">
+                <h1>@ Bupter</h1>
+                <h2 class="cook">尊敬的会员</h2>
+                <h2 class="cook1">欢迎来到波普特</h2>
+                <h2 class="cook1">绿色酒店</h2>
+
+            </div>
+            <div class="rectangle2">
+                <h2 class="attentions">注意事项</h2>
+                <h2 class="attentions1">1. 如有查看订单的需要请在线上或到前台查询</h2>
+                <h2 class="attentions1">2. 本店资源有限，请爱护空调等电子设备</h2>
+                <h2 class="attentions1">3. 请在纳凉人员在的时候尽可能提出需求</h2>
+                <h2 class="attentions1">4. 如有点餐需求请在线上直接进行点单</h2>
+                <h2 class="attentions1">5. 本店提倡友好服务，请尊重工作人员</h2>
+                <h2 class="attentions1">6. 工作人员态度恶劣可到前台处举报</h2>
+                <h2 class="attentions1">7. 如有纳凉需要请向前台提出申请</h2>
+                <h2 class="attentions1">8. 祝您入住愉快</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'UserMain',
+}
+</script>
+
+<style scoped>
+.user-main {
+    background-size: cover;
+    background-position-x: center;
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+    color: #ffffff;
+    overflow: hidden;
+    height: 98.1vh;
+    width: 154vh;
+}
+.rectangle {
+    margin-top: -58%;
+    margin-left: 14vh;
+    margin-right: 20vh;
+    height: 80.5vh;
+    width: 125vh;
+    align-items: center;
+    background-color: #c0e1ea;
+    display: flex;
+    justify-content: space-between;
+}
+.rectangle1 {
+    width: 57vh;
+    height: 70vh;
+    background-color: #e6f0f430;
+    margin-left: 5vh;
+    position: relative;
+    z-index: 10;
+}
+.rectangle2 {
+    width: 57vh;
+    height: 70vh;
+    background-color:#e6f0f430;
+    margin-right: 5vh;
+    position: relative;
+    z-index: 10;
+}
+h1 {
+    text-align: left;
+    margin-left: 3vh;
+}
+.cook{
+    margin-top:8vh;
+    font-size: 6vh;
+    color: #ffffffd3;
+}
+.cook1 {
+    margin-top: 5vh;
+    font-size: 6vh;
+    color: #ffffffd3;
+}
+.attentions{
+    margin-top: 7vh;
+    margin-left: 3vh;
+    margin-bottom: 7vh;
+    font-size: 3.5vh;
+}
+.attentions1 {
+    margin-top: 2vh;
+    text-align: left;
+    font-size: 2.4vh;
+    margin-left: 5vh;
+}
+</style>
---
+++ hotelfront/src/components/UserWindow.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="user-window">
+        <div class="sidebar">
+            <div v-for="(item, index) in sidebarItems" :key="index" 
+                class="sidebar-item" 
+                @click="showPage(index)"
+                :class="{ active: currentPage === index }">
+                {{ item.name }}
+            </div>
+        </div>
+        <div class="content">
+            <component :is="currentPageComponent"></component>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import Page0 from './UserMain.vue';
+import Page1 from './UserCool.vue'; 
+import Page2 from './CustomerSnack.vue';
+import Page3 from './UserLogin.vue';
+
+export default {
+    name: 'UserWindow',
+    data() {
+        return {
+            sidebarItems: [
+                { name: '主页', component: 'Page0' }, 
+                { name: '纳凉服务', component: 'Page1' }, 
+                { name: '进行点餐', component: 'Page2' },
+                { name: '查看订单', component: 'Page3' }
+            ],
+            currentPage: 0 
+        };
+    },
+    computed: {
+        currentPageComponent() {
+            return this.sidebarItems[this.currentPage].component;
+        }
+    },
+    methods: {
+        showPage(index) {
+            this.currentPage = index;
+        }
+    },
+    components: {
+        Page0,
+        Page1,
+        Page2,
+        Page3
+    }
+};
+</script>
+
+<style scoped>
+.user-window {
+    display: flex;
+    height: 97.8vh;
+}
+
+.sidebar {
+    margin-top: 0.3vh;
+    width: 25vh;
+    background-color: #f5f9eb;
+}
+
+.sidebar-item {
+    padding: 15px;
+    cursor: pointer;
+}
+
+.sidebar-item:hover {
+    background-color: #f0e6e6;
+}
+
+.sidebar-item.active {
+    background-color: #bbb;
+}
+
+.content {
+    flex: 1;
+    padding: 2px;
+    background-color: #e2edd9af;
+}
+</style>
