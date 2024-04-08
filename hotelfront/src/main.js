6dca2dd0f1a7adb1aeb3d7013a87901b6826ff77
Rablady <116176222+Rablady@users.noreply.github.com>
2024-04-08 22:08:58 +0800

    Update main.js

--- hotelfront/src/main.js
+++ hotelfront/src/main.js
@@ -4,19 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import UserLogin from './components/UserLogin.vue'
 import CustomerSnack from './components/CustomerSnack.vue'
-import UserView from './components/UserView.vue'
 import UserCool from './components/UserCool.vue'
 import UserWindow from './components/UserWindow.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
 const routes = [
     { path: '/', component: UserLogin },
     { path: '/CustomerSnack', component: CustomerSnack },
-    { path: '/uservieW',component: UserView},
-    { path: '/Usercool',component: UserCool},
-    { path: '/UserWindow',component: UserWindow},
-    
+    { path: '/Usercool', component: UserCool },
+    { path: '/window', component: UserWindow },
+    { path: '/UserMain', component: UserWindow },
 ]
 
 const router = createRouter({
@@ -27,4 +22,3 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-app.use(ElementPlus)
