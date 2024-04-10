214d6e5527f03061b28bf306232eebfeab6aa8c3
hvinci <93910286+hvinci@users.noreply.github.com>
2024-04-11 00:25:02 +0800

    update 开启空调

--- hotelfront/src/components/UserCool.vue
+++ hotelfront/src/components/UserCool.vue
@@ -1,154 +1,165 @@
 <template>
-    <div class="air-conditioner-service">
-      <!-- 放在整个页面的顶部 -->
-      <el-alert v-if="power" title="开启成功" type="success" center show-icon class="floating-alert" />
-      <div class="content">
-        <el-card class="control-panel" shadow="always">
-            <template #header>
-                <div class="card-header">
-                  <span>空调控制面板</span>
-                </div>
-              </template>
-          <!-- 上半部分：温度、风速、空调状态 -->
-          <div class="status-display">
-            <div class="led-row" style="margin-top: 0px;">
-              <span class="subtext">当前温度: </span>
-              <span class="led-digit temperature">{{ currentTemperature }}</span>
-              <span class="led-digit temperature">°C</span>
-            </div>
-            <div class="led-row">
-              <span class="subtext">预期温度: </span>
-              <span class="led-digit temperature">{{ targetTemperature }}</span>
-              <span class="led-digit temperature">°C</span>
-            </div>
-            <div class="led-row" style="margin-top: 40px;">
-              <span class="subtext" style="margin-left: 10px;">风速</span>
-              <span class="subtext fan-speed" style="margin-right: 200px">{{ fanSpeed }}</span>
-              <span class="subtext power-status">{{ power ? '开启' : '关闭' }}</span>
-            </div>
+  <div class="air-conditioner-service">
+    <!-- 放在整个页面的顶部 -->
+    <el-alert v-if="showAlert" title="开启成功" type="success" center show-icon class="floating-alert" />
+    <div class="content">
+      <el-card class="control-panel" shadow="always">
+        <template #header>
+          <div class="card-header">
+            <span>空调控制面板</span>
           </div>
-  
-          <!-- 下半部分：调节按钮 -->
-          <div class="control-buttons">
-            <el-switch v-model="power" inline-prompt active-text="ON" inactive-text="OFF" />
-            <el-button @click="increaseTemperature" :disabled="!power">+</el-button>
-            <el-button @click="decreaseTemperature" :disabled="!power">-</el-button>
-            <el-button @click="adjustFanSpeed" :disabled="!power">调节风速</el-button>
+        </template>
+        <!-- 上半部分：温度、风速、空调状态 -->
+        <div class="status-display">
+          <div class="led-row" style="margin-top: 0px;">
+            <el-text>当前温度: </el-text>
+            <span class="led-digit temperature">{{ currentTemperature }}</span>
+            <span class="led-digit temperature">°C</span>
           </div>
-        </el-card>
-      </div>
+          <div class="led-row">
+            <el-text>预期温度: </el-text>
+            <span class="led-digit temperature">{{ targetTemperature }}</span>
+            <span class="led-digit temperature">°C</span>
+          </div>
+          <div class="led-row" style="margin-top: 40px;">
+            <el-text style="margin-left: 10px;">风速</el-text>
+            <span class="subtext fan-speed" style="margin-right: 200px">{{ fanSpeed }}</span>
+            <el-text tag="b">空调{{ power ? '开启' : '关闭' }}</el-text>
+          </div>
+        </div>
+
+        <!-- 下半部分：调节按钮 -->
+        <div class="control-buttons">
+          <el-switch v-model="power" inline-prompt active-text="ON" inactive-text="OFF" />
+          <el-button @click="increaseTemperature" :disabled="power">+</el-button>
+          <el-button @click="decreaseTemperature" :disabled="power">-</el-button>
+          <el-button @click="adjustFanSpeed" :disabled="power">调节风速</el-button>
+        </div>
+      </el-card>
     </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'
-  
-  export default {
-    name: 'UserCool',
-    setup() {
-      const power = ref(false) // 定义开关状态
-      const showAlert = ref(false) // 定义控制是否显示 alert 的状态
-  
-      // 10秒后自动关闭 alert
-      setTimeout(() => {
-        showAlert.value = false
-      }, 5000)
-  
-      return { power, showAlert } // 返回响应式数据
-    },
-    data() {
-      return {
-        currentTemperature: 25, // 当前温度
-        targetTemperature: 20, // 预期温度
-        fanSpeed: '中速', // 风速
-        accumulatedCost: 0, // 累计费用
-      }
-    },
-    methods: {
-      increaseTemperature() {
-        this.targetTemperature++;
-      },
-      decreaseTemperature() {
-        this.targetTemperature--;
-      },
-      adjustFanSpeed() {
-        // 调节风速的逻辑
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'UserCool',
+  setup() {
+    const power = ref(false) 
+    const showAlert = ref(false) 
+    // TODO: 登录时后端返回temperture
+    const currentTemperature = ref(25) 
+    const targetTemperature = ref(20) 
+    const fanSpeed = ref(1) 
+
+    
+    const ws = new WebSocket('ws://localhost:29010/api/customer/cool/turnOn')
+
+    ws.onmessage = (event) => {
+      const message = JSON.parse(event.data)
+      if (message.code == 200) {
+        showAlert.value = true 
+      } else {
+        console.error(message.message)
+
       }
     }
+
+    // 发送开启空调请求
+    const turnOnAC = () => {
+      const message = {
+        targetTemperature: targetTemperature.value,
+        status: power.value ? fanSpeed.value : 0 // 若开启则返回风速大小，否则返回 0
+      }
+      ws.send(JSON.stringify(message))
+    }
+
+    const increaseTemperature = () => {
+      targetTemperature.value++
+    }
+
+    const decreaseTemperature = () => {
+      targetTemperature.value--
+    }
+
+    const adjustFanSpeed = () => {
+      // 切换风速在 1、2、3 之间循环
+      fanSpeed.value = fanSpeed.value < 3 ? fanSpeed.value + 1 : 1
+    }
+
+    return { power, showAlert, currentTemperature, targetTemperature, fanSpeed, turnOnAC, increaseTemperature, decreaseTemperature, adjustFanSpeed }
   }
-  </script>
-  
-  <style scoped>
-  .air-conditioner-service {
-    height: 100%;
-    background-size: cover;
-    background-position: center;
-    background-color: #ffffff;
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .header {
-    font-size: 24px;
-    font-weight: bold;
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
-  
-  .content {
-    display: flex;
-    justify-content: space-between;
-  }
-  
-  .control-panel{
-    width: 40%;
-    padding: 20px;
-    border-radius: 10px;
-    margin-top:60px;
-    margin-left: 450px;
-    
-    margin-top:13%;
-    
-  }
-  
-  .title {
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 10px;
-  }
-  
-  .status-display {
-    margin-top: 20px;
-  }
-  
-  .led-row {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-  }
-  
-  .subtext {
-    margin-left: 10px;
-  }
-  
-  .control-buttons {
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-between;
-  }
-  
-  .cost-display {
-    margin-top: 20px;
-    font-weight: bold;
-  }
-  
-  
-  .floating-alert {
-    position: fixed;
-    margin:auto;
-    margin-top:9%;
-    margin-left:35%;
-    width: 30%;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.air-conditioner-service {
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+}
+
+.control-panel {
+  width: 40%;
+  padding: 20px;
+  border-radius: 10px;
+  margin-top: 60px;
+  margin-left: 450px;
+  margin-top: 13%;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.status-display {
+  margin-top: 20px;
+}
+
+.led-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.subtext {
+  margin-left: 10px;
+}
+
+.control-buttons {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.cost-display {
+  margin-top: 20px;
+  font-weight: bold;
+}
+
+.floating-alert {
+  position: fixed;
+  margin: auto;
+  margin-top: 9%;
+  margin-left: 35%;
+  width: 30%;
+}
+</style>
